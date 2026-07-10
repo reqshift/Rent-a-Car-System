8188caf0cf5c310369da217f2aa608c2255f959a
reqshift <reqshiftt@gmail.com>
2026-07-10 19:58:57 +0300

    refactor(exception): improve InvalidRentalResultException
    Refined the InvalidRentalResultException implementation.
    
    - Improved exception structure
    - Aligned with the project exception hierarchy
    - Increased code consistency and readability

--- src/main/java/com/staj/rentacar/exception/InvalidRentalResultException.java
+++
@@ -1,7 +0,0 @@
-package com.staj.rentacar.exception;
-
-public class InvalidRentalResultException extends RentalException { //result==null için onu return false yerine hata teşkil ettiği için runtime exception ile kullanacağım
-    public InvalidRentalResultException(String message) {
-        super(message);
-    }
-}
